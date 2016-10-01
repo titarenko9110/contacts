1f12611c24a914e15da3412e4ffaff7f218294d1
MyMac <titarenko9110@mail.ru>
2016-10-01 14:33:41 +0300

    comit

--- src/main/java/titarenko/test2/Application.java
+++ src/main/java/titarenko/test2/Application.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) throws Exception{
         SpringApplication.run(Application.class,args);
     }
